07d39171175711222b86db7f6019f2c55a6af038
Allen Bai <abai@redhat.com>
2021-06-30 12:14:44 -0400

    workflows: add redhat and community workflow logic
    As part of supporting different vendor types for openshift helm
    charts, this adds the github actions for redhat and community
    submission. For design details please refer to: docs/workflow-build.md.
    
    Signed-off-by: Allen Bai <abai@redhat.com>

--- scripts/src/chartrepomanager/chartrepomanager.py
+++ scripts/src/chartrepomanager/chartrepomanager.py
@@ -128,6 +128,16 @@ def create_index_from_chart(indexdir, repository, branch, category, organization
     print(p)
     print(out.stderr.decode("utf-8"))
     crt = yaml.load(p, Loader=Loader)
+    annotations = crt["annotations"]
+    redhat_to_community = bool(os.environ.get("REDHAT_TO_COMMUNITY"))
+    if category == "partners":
+        annotations["charts.openshift.io/providerType"] = "partner"
+    elif category == "redhat" and redhat_to_community:
+        annotations["charts.openshift.io/providerType"] = "community"
+    else:
+        annotations["charts.openshift.io/providerType"] = category
+    crt["annotations"] = annotations
+
     return crt
 
 def create_index_from_report(category, report_path):
